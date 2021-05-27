af278e8cf91f0c29d1106530c8251ad208c37e70
Grant Kinney <hi@grant.mk>
2021-05-26 20:58:33 -0500

    Correct title in e2e test

--- packages/e2e-tests/specs/experiments/post-editor-template-mode.test.js
+++ packages/e2e-tests/specs/experiments/post-editor-template-mode.test.js
@@ -51,7 +51,7 @@ const switchToTemplateMode = async () => {
 		'.edit-post-template-top-area',
 		( el ) => el.innerText
 	);
-	expect( title ).toContain( 'Another FSE Post\n' );
+	expect( title ).toContain( 'Just an FSE Post\n' );
 
 	await disableTemplateWelcomeGuide();
 };
